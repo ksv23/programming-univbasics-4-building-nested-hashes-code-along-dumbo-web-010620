f0163151c404efa7d3d622ee39bbd9f4a185ce2e
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-24 05:42:45 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :name => {name: "Benvolio", age: "17", attitude:"worried"}
+        :name => {name: "Benvolio", age: "17", attitude:"worried"},
         :name => {name: "Mercutio", age: "18", attitude:"hot-headed"}
       ]
    },
@@ -14,7 +14,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :name => {name: "Steven", age: "30", attitude:"confused"}
+        :name => {name: "Steven", age: "30", attitude:"confused"},
         :name => {name: "Nurse", age: "44", attitude:"worried"}
       ]
    }
